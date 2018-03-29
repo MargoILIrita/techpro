25e776593b313f246cb1472f243d90e0fe9f9f52
Mihail063 <19680cm@gmail.com>
2018-03-29 13:48:33 +0400

    GUI v1.1

--- GUI.py
+++ GUI.py
@@ -11,7 +11,8 @@ from tkinter import ttk
 #import legacycode
 
 LARGE_FONT = ("Verdana", 12)
-
+global n
+n = 0
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -26,56 +27,50 @@ class SeaofBTCapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, Graph):
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
+        frame = StartPage(container, self)
+        frame.grid(row=0, column=0, sticky="nsew")
         frame.tkraise()
 
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
+        def graph():
+            label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+            label.pack(pady=10, padx=10)
+
+            button1 = ttk.Button(self, text="Back to Home",
+                                 command=lambda: controller.show_frame(StartPage))
+            button1.pack()
+            x = np.arange(-10, 11, 1)
+            y = x
+            f = Figure(figsize=(5, 5), dpi=100)
+            a = f.add_subplot(111)
+            global n
+            print(n)
+            a.plot(x, y)
+            canvas = FigureCanvasTkAgg(f, self)
+            canvas.show()
+            canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+            toolbar = NavigationToolbar2TkAgg(canvas, self)
+            toolbar.update()
+            canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        def click_btn():
+            global n
+            n = nEntry.get()
+            graph()
+
         tk.Frame.__init__(self, parent)
 
         tk.Label(self, text="Start Page", font=LARGE_FONT).pack(pady=10, padx=10)
-        ttk.Button(self, text="Graph", command=lambda: controller.show_frame(Graph)).pack()
+        nEntry = tk.Entry(self, width = 20)
+        nEntry.pack()
+        btn = ttk.Button(self, text="Graph", command = click_btn)
         tk.Radiobutton(self, text="GraphRK3", value = 1).pack()
         tk.Radiobutton(self, text="GraphRK4", value = 2).pack()
         tk.Radiobutton(self, text="GraphAll", value = 3).pack()
-        nEntery = tk.Entry(self, text = "Enter you N", width = 20).pack()
-        nEntery.get()
-        n = int(nEntery)
-
-
-class Graph(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-        x = np.arange(-10,11, 1)
-        y = x
-        f = Figure(figsize=(5, 5), dpi=100)
-        a = f.add_subplot(111)
-        a.plot(x,y)
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        btn.pack()
 
 
 app = SeaofBTCapp()
