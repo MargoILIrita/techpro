d23d72b12e369acd92835e5f4a365a4d0f5e5f88
Mihail063 <19680cm@gmail.com>
2018-04-20 12:53:31 +0400

    edit GUI

--- GUI.py
+++ GUI.py
@@ -1,18 +1,7 @@
 import matplotlib.pyplot as mtp
-import numpy as np
-import pylab
-
 import tkinter as tk
 from tkinter import ttk
 import legacycode
-
-LARGE_FONT = ("Verdana", 12)
-global n
-n = 0
-global x
-global y
-x = 0
-y = 0
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -21,40 +10,42 @@ class App(tk.Tk):
         tk.Tk.wm_title(self, "Sea of BTC client")
 
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         def graph():
-            n = int(nEntry.get())
-            if n != 0:
-                print(n)
-            fn = type.get()
-            f = fn[0]
-            if f == "0":
-                x,y = legacycode.thirdfordraw(n,n)
-                leg1 = mtp.plot(x, y)
-                mtp.legend(leg1,"Метод Рунге-Кутта 3 порядка")
-                mtp.grid()
-                mtp.show()
+            if nEntry.get() != '' and endEntry.get() != '':
+                n = int(nEntry.get())
+                end = int(endEntry.get())
+                fn = type.get()
+                f = fn[0]
+                if f == "0":
+                    x,y = legacycode.thirdfordraw(end,n)
+                    leg1 = mtp.plot(x, y)
+                    mtp.legend(leg1,"Метод Рунге-Кутта 3 порядка")
+                    mtp.grid()
+                    mtp.show()
 
-            if f == "1":
-                x, y = legacycode.forthfordraw(n, n)
-                leg1 = mtp.plot(x, y)
-                mtp.legend(leg1,"Метод Рунге-Кутта 4 порядка")
-                mtp.grid()
-                mtp.show()
-            if f == "2":
-                x, y = legacycode.thirdfordraw(n, n)
-                x1, y1 = legacycode.forthfordraw(n, n)
-                leg1,leg2 = mtp.plot(x, y, x, y1)
-                mtp.legend((leg1,leg2),("Метод Рунге-Кутта 3 порядка","Метод Рунге-Кутта 4 порядка"))
-                mtp.grid()
-                mtp.show()
+                if f == "1":
+                    x, y = legacycode.forthfordraw(end,n)
+                    leg1 = mtp.plot(x, y)
+                    mtp.legend(leg1,"Метод Рунге-Кутта 4 порядка")
+                    mtp.grid()
+                    mtp.show()
+                if f == "2":
+                    x, y = legacycode.thirdfordraw(end,n)
+                    x1, y1 = legacycode.forthfordraw(end,n)
+                    leg1,leg2 = mtp.plot(x, y, x, y1)
+                    mtp.legend((leg1,leg2),("Метод Рунге-Кутта 3 порядка","Метод Рунге-Кутта 4 порядка"))
+                    mtp.grid()
+                    mtp.show()
 
-
+        tk.Label(self, text = "N = ").grid(row=1)
         nEntry = tk.Entry(self, width=20)
-        nEntry.pack(side=tk.LEFT)
+        nEntry.grid(row=1,column=1)
+        tk.Label(self, text="End = ").grid(row=2)
+        endEntry = tk.Entry(self, width=20)
+        endEntry.grid(row=2,column=1)
         #
         type = tk.StringVar()
         type.set('0 Метод Рунге-Кутта 3 порядка')
@@ -62,9 +53,9 @@ class App(tk.Tk):
                 '0 Метод Рунге-Кутта 3 порядка',
                 '1 Метод Рунге-Кутта 4 порядка',
                 '2 Оба графика')
-        fspis.pack({"side": "left"})
+        fspis.grid(row=1,column=3)
         btn = ttk.Button(self, text="Graph", command=graph)
-        btn.pack(side=tk.LEFT)
+        btn.grid(row=1,column=4)
 
 
 app = App()
