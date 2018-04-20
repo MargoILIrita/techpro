cd76742903a847c25a5a692dfc48843e5d8d1f05
Margarita <rita96pol@gmail.com>
2018-04-21 00:56:22 +0400

    try to setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+setup(
+    name='The Runge–Kutta method',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Margarita Polianskaia, Mikhail Silkin',
+    author_email='rita96pol@gmail.com',
+    description=''
+)
