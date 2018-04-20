4e2ef3862e356f109d6e65f1589d318502601b9c
Margarita <rita96pol@gmail.com>
2018-04-20 13:05:01 +0400

    tests

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+import legacycode as l
+
+class MyTestCase(unittest.TestCase):
+    def test_derivative(self):
+        self.assertEqual(l.derivative(0,0), 0)
+        self.assertEqual(l.derivative(1,0), 0)
+        self.assertEqual(l.derivative(1,1), 0)
+        self.assertEqual(l.derivative(2,3), -3.5)
+        self.assertNotEqual(l.derivative(0,0),1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
